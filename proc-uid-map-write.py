c3e3a37100d3634bd7fdd7f2b71246191fb04542
Ciro S. Costa <cscosta@pivotal.io>
2019-11-24 15:40:39 +0000

    display userns at ns creation time
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- proc-uid-map-write.py
+++ proc-uid-map-write.py
@@ -11,39 +11,51 @@ program = BPF(
 #include <uapi/linux/ptrace.h>
 
 
-static inline void
-show_uid_map(struct uid_gid_map* mapping)
+static inline int
+__print_uid_map(struct pt_regs* ctx)
 {
-        bpf_trace_printk("FIRST\tLOWER\tCOUNT\n");
-        bpf_trace_printk("%d\t%d\t%d\n",
-                         mapping->extent[0].first,
-                         mapping->extent[0].lower_first,
-                         mapping->extent[0].count);
-}
-
-int
-kr__proc_uid_map_write(struct pt_regs* ctx)
-{
-
         struct task_struct* task;
         struct uid_gid_map* mapping;
 
         task = (struct task_struct*)bpf_get_current_task();
+        mapping = &task->cred->user_ns->uid_map;
 
-        if (task->cred->user_ns->uid_map.nr_extents < 1) {
+        if (mapping->nr_extents < 1) {
                 return 0;
         }
 
-        mapping = &task->cred->user_ns->uid_map;
-        show_uid_map(mapping);
+        bpf_trace_printk("%d,%d,%d\n",
+                         mapping->extent[0].first,
+                         mapping->extent[0].lower_first,
+                         mapping->extent[0].count);
 
         return 0;
+}
+
+
+int
+kr__create_user_ns(struct pt_regs* ctx)
+{
+    bpf_trace_printk("create_user_ns\n");
+    return __print_uid_map(ctx);
+}
+
+
+int
+kr__proc_uid_map_write(struct pt_regs* ctx)
+{
+    bpf_trace_printk("proc_uid_map\n");
+    return __print_uid_map(ctx);
 }
 """
 )
 
 program.attach_kretprobe(
     event="proc_uid_map_write", fn_name="kr__proc_uid_map_write"
+)
+
+program.attach_kretprobe(
+    event="create_user_ns", fn_name="kr__create_user_ns"
 )
 
 program.trace_print()
